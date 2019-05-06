2284ae7a8de5d2de5c1a49bbb7471ce7eeec6409
Vitor Emanuel <veslima3@gmail.com>
2019-05-05 22:08:03 -0300

    Finders Keepers
    Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
    
    Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.

--- Finders Keepers.js
+++ Finders Keepers.js
@@ -1,9 +1,12 @@
 function findElement(arr, func) {
-  let num = arr.filter(func);
-  if (arr.length >= 1){
-    return num [0];
+  let num = 0;
+  
+  for(var i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
   }
+  
   return undefined;
 }
-
-findElement([1, 2, 3, 4], num => num % 2 === 0);
